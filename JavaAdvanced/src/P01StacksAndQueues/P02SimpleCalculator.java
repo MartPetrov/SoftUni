8d88d69ad74d9f1c7aa3b053e88e0ad44f451f49
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2022-03-22 00:53:19 +0200

    StacksAndQueues

---
+++ JavaAdvanced/src/P01StacksAndQueues/P02SimpleCalculator.java
@@ -0,0 +1,29 @@
+package P01StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class P02SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input =  scanner.nextLine().split(" ");
+        ArrayDeque<String> tokens = new ArrayDeque<>();
+        Collections.addAll(tokens, input);
+
+        while (tokens.size() > 1) {
+            int firstNum = Integer.parseInt(tokens.pop());
+            String operation = tokens.pop();
+            int secondNum = Integer.parseInt(tokens.pop());
+
+            int result = 0;
+            if (operation.equals("+")) {
+                result = firstNum + secondNum;
+            }else {
+                result = firstNum - secondNum;
+            }
+            tokens.push(String.valueOf(result));
+        }
+        System.out.println(tokens.peek());
+    }
+}
---
+++ JavaAdvanced/src/P01StacksAndQueues/P05PrinterQueue.java
@@ -0,0 +1,30 @@
+package P01StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P05PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        ArrayDeque<String> fileRequest = new ArrayDeque<>();
+
+        while (!input.equals("print")) {
+            if (input.equals("cancel")) {
+                if (fileRequest.isEmpty()) {
+                    System.out.printf("Printer is on standby%n");
+                } else {
+                    String firstFile = fileRequest.poll();
+                    System.out.printf("Canceled %s%n", firstFile);
+                }
+            } else {
+                fileRequest.offer(input);
+            }
+            input = scanner.nextLine();
+        }
+        for (String elementInQueue : fileRequest) {
+            System.out.println(elementInQueue);
+        }
+    }
+}
