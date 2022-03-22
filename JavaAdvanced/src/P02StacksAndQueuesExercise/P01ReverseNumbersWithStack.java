17b5d49090ec41b1fe1082fafd1e76b530d3fbb4
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2022-03-23 01:09:39 +0200

    StacksAndQueuesExercise

---
+++ JavaAdvanced/src/P02StacksAndQueuesExercise/P01ReverseNumbersWithStack.java
@@ -0,0 +1,23 @@
+package P02StacksAndQueuesExercise;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P01ReverseNumbersWithStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(" ");
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        for (String number : input) {
+            stack.push(number);
+        }
+        while (stack.size() > 1) {
+            System.out.print(stack.pop() + " ");
+        }
+        System.out.println(stack.peek());
+    }
+}
+
---
+++ JavaAdvanced/src/P02StacksAndQueuesExercise/P04BasicQueueOperations.java
@@ -0,0 +1,34 @@
+package P02StacksAndQueuesExercise;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class P04BasicQueueOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int s = scanner.nextInt();
+        int x = scanner.nextInt();
+
+        ArrayDeque<Integer> queues = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            queues.offer(scanner.nextInt());
+        }
+        for (int i = 1; i <= s; i++) {
+            queues.poll();
+        }
+
+        if (queues.contains(x)) {
+            System.out.println("true");
+        } else {
+            if (queues.size() == 0) {
+                System.out.println("0");
+            } else {
+                System.out.println(Collections.min(queues));
+            }
+        }
+
+    }
+}
