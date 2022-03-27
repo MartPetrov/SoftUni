a834646195914af86ea81a4dd0a070448d02ec03
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2022-03-28 00:40:06 +0300

    Exam05For2Time

---
+++ SoftUni JavaFundamentals/src/Exam05For2Time/P01ActivationKeys.java
@@ -0,0 +1,48 @@
+package Exam05For2Time;
+
+import java.util.Scanner;
+
+public class P01ActivationKeys {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String message = scanner.nextLine();
+        String command = scanner.nextLine();
+
+        while (!command.equals("Generate")) {
+            String[] commandArr = command.split(">>>");
+            switch (commandArr[0]) {
+                case "Contains":
+                    String substring = commandArr[1];
+                    if (message.contains(substring)) {
+                        System.out.printf("%s contains %s%n", message, substring);
+                    } else {
+                        System.out.println("Substring not found!");
+                    }
+                    break;
+                case "Flip":
+                    String type = commandArr[1];
+                    int startIndex = Integer.parseInt(commandArr[2]);
+                    int endIndex = Integer.parseInt(commandArr[3]);
+                    String subStringFromTo = message.substring(startIndex,endIndex);
+                    if (type.equals("Upper")) {
+                        message = message.replace(subStringFromTo,subStringFromTo.toUpperCase());
+                    } else if (type.equals("Lower")) {
+                        message = message.replace(subStringFromTo,subStringFromTo.toLowerCase());
+                    }
+                    System.out.println(message);
+                    break;
+                case "Slice":
+                    int startIndexToDelete = Integer.parseInt(commandArr[1]);
+                    int endIndexToDelete = Integer.parseInt(commandArr[2]);
+                    String subStringFromToToDelete = message.substring(startIndexToDelete,endIndexToDelete);
+                    message = message.replace(subStringFromToToDelete,"");
+                    System.out.println(message);
+                    break;
+
+            }
+            command = scanner.nextLine();
+        }
+        System.out.printf("Your activation key is: %s", message);
+    }
+}
---
+++ SoftUni JavaFundamentals/src/Exam05For2Time/P03Pirates.java
@@ -0,0 +1,76 @@
+package Exam05For2Time;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class P03Pirates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Map<String, Integer> populationMap = new LinkedHashMap<>();
+        Map<String, Integer> goldMap = new LinkedHashMap<>();
+
+        while (!input.equals("Sail")) {
+            String[] inputArr = input.split("\\|\\|");
+            if (populationMap.containsKey(inputArr[0])) {
+                int currentPop = populationMap.get(inputArr[0]);
+                int currentGold = goldMap.get(inputArr[0]);
+                populationMap.put(inputArr[0],currentPop + Integer.parseInt(inputArr[1]));
+                goldMap.put(inputArr[0], currentGold + Integer.parseInt(inputArr[2]));
+            } else {
+                populationMap.put(inputArr[0], Integer.parseInt(inputArr[1]));
+                goldMap.put(inputArr[0], Integer.parseInt(inputArr[2]));
+            }
+
+
+            input = scanner.nextLine();
+        }
+
+        String inputCommand = scanner.nextLine();
+
+        while (!inputCommand.equals("End")) {
+            String[] command = inputCommand.split("=>");
+            if (command[0].equals("Plunder")) {
+                String currentCity = command[1];
+                int killedPeople = Integer.parseInt(command[2]);
+                int stolenGold = Integer.parseInt(command[3]);
+                int currentPopulation = populationMap.get(currentCity);
+                int currentGold = goldMap.get(currentCity);
+                populationMap.put(currentCity,currentPopulation - killedPeople);
+                goldMap.put(currentCity,currentGold - stolenGold);
+                System.out.printf("%s plundered! %d gold stolen, %d citizens killed.%n", currentCity,stolenGold,killedPeople);
+
+                if (populationMap.get(currentCity)<=0 || goldMap.get(currentCity) <= 0) {
+                    populationMap.remove(currentCity);
+                    goldMap.remove(currentCity);
+                    System.out.printf("%s has been wiped off the map!%n", currentCity);
+                }
+
+            } else if (command[0].equals("Prosper")) {
+                String currentCity = command[1];
+                int goldToProsper = Integer.parseInt(command[2]);
+                if (goldToProsper < 0) {
+                    System.out.printf("Gold added cannot be a negative number!%n");
+                    inputCommand = scanner.nextLine();
+                    continue;
+                }
+                int currentGold = goldMap.get(currentCity);
+                goldMap.put(currentCity,currentGold + goldToProsper);
+                int goldAfterAdd = goldMap.get(currentCity);
+                System.out.printf("%d gold added to the city treasury. %s now has %d gold.%n", goldToProsper,currentCity,goldAfterAdd);
+            }
+
+            inputCommand = scanner.nextLine();
+        }
+        if (populationMap.isEmpty()) {
+            System.out.println("Ahoy, Captain! All targets have been plundered and destroyed!");
+        } else {
+            System.out.printf("Ahoy, Captain! There are %d wealthy settlements to go to:%n", populationMap.size());
+            populationMap.entrySet().forEach
+                    (e -> System.out.printf("%s -> Population: %d citizens, Gold: %d kg%n",e.getKey(),e.getValue(),goldMap.get(e.getKey())));
+        }
+    }
+}
