2e1ac8f23caab758088709bbf1defd3ba9125a60
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2022-04-15 21:57:11 +0300

    Add Algorithms Fund

--- Algorithms Fundamentals with Java/src/L01RecursionAndBacktrackingLab/P01RecursiveArraySum.java
+++ Algorithms Fundamentals with Java/src/L01RecursionAndBacktrackingLab/P01RecursiveArraySum.java
@@ -1,8 +1,10 @@
-package P01RecursionAndBacktracking;
+package L01RecursionAndBacktrackingLab;
+
+import java.util.Scanner;
 
 public class P01RecursiveArraySum {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
     }
 }
