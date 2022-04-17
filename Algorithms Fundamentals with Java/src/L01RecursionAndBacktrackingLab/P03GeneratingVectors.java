104e82d11b228c86dcdee3974f4751db594273f4
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2022-04-17 13:10:29 +0300

    Add some exercise from Recursion And Backtracking Lab

---
+++ Algorithms Fundamentals with Java/src/L01RecursionAndBacktrackingLab/P03GeneratingVectors.java
@@ -0,0 +1,9 @@
+package L01RecursionAndBacktrackingLab;
+
+import java.util.Scanner;
+
+public class P03GeneratingVectors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
