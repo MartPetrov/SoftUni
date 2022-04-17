07f3ae780b9a7bfa5ce80d50c115c5e0b560a60f
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2022-04-17 14:02:17 +0300

    Clear

--- Algorithms Fundamentals with Java/src/L01RecursionAndBacktrackingLab/P03GeneratingVectors.java
+++
@@ -1,9 +0,0 @@
-package L01RecursionAndBacktrackingLab;
-
-import java.util.Scanner;
-
-public class P03GeneratingVectors {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    }
-}
