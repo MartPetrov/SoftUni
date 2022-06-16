267b8833d0372e32db06dc305278879568344c25
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2022-06-16 22:29:22 +0300

    ExamFrom14.04.2021

--- JavaAdvanced/src/ExamPrep/P02/P02CookingJourney.java
+++ JavaAdvanced/src/ExamPrep/P02/P02CookingJourney.java
@@ -1,171 +1,102 @@
 package ExamPrep.P02;
 
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class P02CookingJourney {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int size = Integer.parseInt(scanner.nextLine());
-
         String[][] matrix = new String[size][size];
 
-        fillMatrix(scanner, size, matrix);
+        fillMatrix(matrix, scanner);
+        int[] coordPlayer = new int[2];
+        findPlayer(matrix, coordPlayer, size);
 
-        int[] coordinate = new int[2];
-
-
-        findPlayer(size, matrix, coordinate);
         int money = 0;
         boolean isOut = false;
         while (money < 50 && !isOut) {
             String command = scanner.nextLine();
-            String current = "";
+            matrix[coordPlayer[0]][coordPlayer[1]] = "-";
             switch (command) {
                 case "up":
-                    matrix[coordinate[0]][coordinate[1]] = "-";
-                    coordinate[0]--;
-                    if (isValidCoordinate(coordinate[0], coordinate[1], matrix)) {
-                        current = matrix[coordinate[0]][coordinate[1]];
-                        if (Character.isDigit(current.charAt(0))) {
-                            money += Integer.parseInt(current);
-                            matrix[coordinate[0]][coordinate[1]] = "S";
-                        }
-                        if (current.equals("P")) {
-                            matrix[coordinate[0]][coordinate[1]] = "-";
-                            for (int row = 0; row < size; row++) {
-                                for (int col = 0; col < size; col++) {
-                                    if (matrix[row][col].equals("P")) {
-                                        matrix[row][col] = "S";
-                                        coordinate[0] = row;
-                                        coordinate[1] = col;
-                                    }
-                                }
-                            }
-                        }
-                    } else if (!isValidCoordinate(coordinate[0], coordinate[1], matrix)) {
-                        isOut = true;
-                    }
+                    coordPlayer[0]--;
                     break;
                 case "down":
-                    matrix[coordinate[0]][coordinate[1]] = "-";
-                    coordinate[0]++;
-
-                    if (isValidCoordinate(coordinate[0], coordinate[1], matrix)) {
-                        current = matrix[coordinate[0]][coordinate[1]];
-                        if (Character.isDigit(current.charAt(0))) {
-                            money += Integer.parseInt(current);
-                            matrix[coordinate[0]][coordinate[1]] = "S";
-                        }
-                        if (current.equals("P")) {
-                            matrix[coordinate[0]][coordinate[1]] = "-";
-                            for (int row = 0; row < size; row++) {
-                                for (int col = 0; col < size; col++) {
-                                    if (matrix[row][col].equals("P")) {
-                                        matrix[row][col] = "S";
-                                        coordinate[0] = row;
-                                        coordinate[1] = col;
-                                    }
-                                }
-                            }
-                        }
-                    } else if (!isValidCoordinate(coordinate[0], coordinate[1], matrix)) {
-                        isOut = true;
-                        break;
-                    }
+                    coordPlayer[0]++;
                     break;
                 case "left":
-                    matrix[coordinate[0]][coordinate[1]] = "-";
-                    coordinate[1]--;
-
-                    if (isValidCoordinate(coordinate[0], coordinate[1], matrix)) {
-                        current = matrix[coordinate[0]][coordinate[1]];
-                        if (Character.isDigit(current.charAt(0))) {
-                            money += Integer.parseInt(current);
-                            matrix[coordinate[0]][coordinate[1]] = "S";
-                        }
-                        if (current.equals("P")) {
-                            matrix[coordinate[0]][coordinate[1]] = "-";
-                            for (int row = 0; row < size; row++) {
-                                for (int col = 0; col < size; col++) {
-                                    if (matrix[row][col].equals("P")) {
-                                        matrix[row][col] = "S";
-                                        coordinate[0] = row;
-                                        coordinate[1] = col;
-                                    }
-                                }
-                            }
-                        }
-                    } else if (!isValidCoordinate(coordinate[0], coordinate[1], matrix)) {
-                        isOut = true;
-                        break;
-                    }
+                    coordPlayer[1]--;
                     break;
                 case "right":
-                    matrix[coordinate[0]][coordinate[1]] = "-";
-                    coordinate[1]++;
-                    if (isValidCoordinate(coordinate[0], coordinate[1], matrix)) {
-                        current = matrix[coordinate[0]][coordinate[1]];
-                        if (Character.isDigit(current.charAt(0))) {
-                            money += Integer.parseInt(current);
-                            matrix[coordinate[0]][coordinate[1]] = "S";
-                        }
-                        if (current.equals("P")) {
-                            matrix[coordinate[0]][coordinate[1]] = "-";
-                            for (int row = 0; row < size; row++) {
-                                for (int col = 0; col < size; col++) {
-                                    if (matrix[row][col].equals("P")) {
-                                        matrix[row][col] = "S";
-                                        coordinate[0] = row;
-                                        coordinate[1] = col;
-                                    }
-                                }
-                            }
-                        }
-                    } else if (!isValidCoordinate(coordinate[0], coordinate[1], matrix)) {
-                        isOut = true;
-                        break;
-                    }
+                    coordPlayer[1]++;
                     break;
             }
 
+            if (isValidCoord(coordPlayer[0], coordPlayer[1], matrix)) {
+                if (Character.isDigit(matrix[coordPlayer[0]][coordPlayer[1]].charAt(0))) {
+                    money += Integer.parseInt(matrix[coordPlayer[0]][coordPlayer[1]]);
+                    matrix[coordPlayer[0]][coordPlayer[1]] = "S";
+                }
+                if (matrix[coordPlayer[0]][coordPlayer[1]].equals("P")) {
+                    matrix[coordPlayer[0]][coordPlayer[1]] = "-";
+                    for (int row = 0; row < matrix.length; row++) {
+                        for (int col = 0; col < matrix.length; col++) {
+                            if (matrix[row][col].equals("P")) {
+                                matrix[row][col] = "S";
+                                coordPlayer[0] = row;
+                                coordPlayer[1] = col;
+                            }
+                        }
+                    }
+                }
+
+            } else {
+                isOut = true;
+                System.out.println("Bad news! You are out of the pastry shop.");
+                break;
+            }
         }
-        if (isOut) {
-            System.out.println("Bad news! You are out of the pastry shop.");
-        } else  {
+
+
+        if (!isOut) {
             System.out.println("Good news! You succeeded in collecting enough money!");
         }
-        System.out.printf("Money: %d%n", money);
-        for (String[] strings : matrix) {
-            for (String string : strings) {
-                System.out.print(string.replaceAll("[\\[\\],]", ""));
+        System.out.printf("Money: %d%n",money);
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                System.out.print(matrix[row][col]);
             }
             System.out.println();
         }
+
     }
 
-    private static boolean isValidCoordinate(int rowCoord, int colCoord, String[][] matrix) {
-        return rowCoord >= 0 && rowCoord < matrix.length && colCoord >= 0 && colCoord < matrix.length;
+    private static boolean isValidCoord(int coord0, int coord1, String[][] matrix) {
+        return (coord0 >= 0 && coord0 < matrix.length && coord1 >= 0 && coord1 < matrix.length);
     }
 
-    private static void findPlayer(int size, String[][] matrix, int[] coordinate) {
+    private static void findPlayer(String[][] matrix, int[] coordPlayer, int size) {
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
                 if (matrix[row][col].equals("S")) {
-                    coordinate[0] = row;
-                    coordinate[1] = col;
+                    coordPlayer[0] = row;
+                    coordPlayer[1] = col;
                 }
+
+            }
+
+        }
+    }
+
+    private static void fillMatrix(String[][] matrix, Scanner scanner) {
+        for (int row = 0; row < matrix.length; row++) {
+            String[] rowInput = scanner.nextLine().split("");
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = rowInput[col];
             }
         }
     }
 
-    private static void fillMatrix(Scanner scanner, int size, String[][] matrix) {
-        IntStream.range(0, size).forEach(row -> {
-            String[] inputMatrix = scanner.nextLine().split("");
-            System.arraycopy(inputMatrix, 0, matrix[row], 0, size);
-        });
-    }
+
 }
