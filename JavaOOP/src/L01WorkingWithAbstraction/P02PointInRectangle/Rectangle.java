a0445f02f4b528509b7cb9a873ad02398268f669
Martin Petrov <Martinpetrow93@gmail.com>
2022-06-20 15:55:22 +0300

    P02PointInRectangle

---
+++ JavaOOP/src/L01WorkingWithAbstraction/P02PointInRectangle/Rectangle.java
@@ -0,0 +1,27 @@
+package L01WorkingWithAbstraction.P02PointInRectangle;
+
+public class Rectangle {
+   private Point bottomLeft;
+   private Point topRight;
+
+
+    public Rectangle(Point bottomLeft, Point topRight) {
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+    }
+
+
+    public Point getBottomLeft() {
+        return bottomLeft;
+    }
+
+    public Point getTopRight() {
+        return topRight;
+    }
+
+    public boolean contains (Point point) {
+        boolean containsOnX = point.getX() >= bottomLeft.getX() && point.getX() <= topRight.getX();
+        boolean containsOnY = point.getY() >= bottomLeft.getY() && point.getY() <= topRight.getY();
+        return containsOnX && containsOnY;
+    }
+}
