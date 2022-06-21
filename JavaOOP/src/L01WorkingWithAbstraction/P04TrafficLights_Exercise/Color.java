10b2aa6407da6ec8627ff8f9f4fd026afa4298fe
Martin Petrov <Martinpetrow93@gmail.com>
2022-06-21 14:32:21 +0300

    P04TraficLights

---
+++ JavaOOP/src/L01WorkingWithAbstraction/P04TrafficLights_Exercise/Color.java
@@ -0,0 +1,7 @@
+package L01WorkingWithAbstraction.P04TrafficLights_Exercise;
+
+public enum Color {
+    RED,
+    YELLOW,
+    GREEN
+}
---
+++ JavaOOP/src/L01WorkingWithAbstraction/P04TrafficLights_Exercise/Main.java
@@ -0,0 +1,30 @@
+package L01WorkingWithAbstraction.P04TrafficLights_Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] colors = scanner.nextLine().split("\\s+");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        List<Light> trafficLight = new ArrayList<>();
+
+        for (String color: colors) {
+            Light currentLight = new Light(Color.valueOf(color));
+            trafficLight.add(currentLight);
+        }
+
+        for (int i = 0; i < n; i++) {
+            trafficLight.forEach(light -> {
+                light.changeColor();
+                System.out.print(light.getColor()+" ");
+            });
+            System.out.println();
+        }
+    }
+}
