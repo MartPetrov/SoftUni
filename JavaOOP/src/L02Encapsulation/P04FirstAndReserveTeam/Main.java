30d4e9c94ea47c23efaa7df20f184a759d584670
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2022-06-22 00:56:39 +0300

    P04FirstAndReserveTeam

---
+++ JavaOOP/src/L02Encapsulation/P04FirstAndReserveTeam/Main.java
@@ -0,0 +1,23 @@
+package L02Encapsulation.P04FirstAndReserveTeam;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfPeople = Integer.parseInt(scanner.nextLine());
+        Team team = new Team("Black Eagles");
+
+        for (int i = 0; i < numberOfPeople; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            Person personToAdd = new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3]));
+            team.addPlayer(personToAdd);
+        }
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getFirstTeam().size() + " players");
+    }
+}
+
+
+
+
