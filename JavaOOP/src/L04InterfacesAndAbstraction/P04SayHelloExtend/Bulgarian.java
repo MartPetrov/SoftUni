96c90f5fd893da4cf652bc9e9fcec43836cfd6e8
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2022-06-27 23:06:28 +0300

    P03AndP04

---
+++ JavaOOP/src/L04InterfacesAndAbstraction/P04SayHelloExtend/Bulgarian.java
@@ -0,0 +1,13 @@
+package L04InterfacesAndAbstraction.P04SayHelloExtend;
+
+public class Bulgarian extends BasePerson {
+
+    public Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
---
+++ JavaOOP/src/L04InterfacesAndAbstraction/P04SayHelloExtend/Person.java
@@ -0,0 +1,6 @@
+package L04InterfacesAndAbstraction.P04SayHelloExtend;
+
+public interface Person {
+    String getName();
+    String sayHello();
+}
